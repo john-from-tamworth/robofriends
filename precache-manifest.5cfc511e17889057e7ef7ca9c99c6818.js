0f9b7ea1caf7a08df363206f1bf04c755c6e3496
John Hands <johnhands@Johns-MacBook-Pro.local>
2020-01-30 00:40:01 +0000

    Updates

---
+++ precache-manifest.5cfc511e17889057e7ef7ca9c99c6818.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "696a90ee8a9afdb699934e81ac75870c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "ef33967d85963ca12d00",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f35787e381d046587084",
+    "url": "/robofriends/static/css/main.88676fb7.chunk.css"
+  },
+  {
+    "revision": "ef33967d85963ca12d00",
+    "url": "/robofriends/static/js/2.b0983e84.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.b0983e84.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f35787e381d046587084",
+    "url": "/robofriends/static/js/main.f4e135a8.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
